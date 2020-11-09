865811c4032a81afb44ce6ff1eaec7d22dfe1356
Igor <igor.ushkarev@digital.justice.gov.uk>
2020-11-09 17:14:54 +0000

    Remove support for python 3.4 because django 2.2. doesn't support it

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',

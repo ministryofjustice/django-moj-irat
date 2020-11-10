d0160c595a85285f60760e0d1d4afcbd7ab48a26
Igor <igor.ushkarev@digital.justice.gov.uk>
2020-11-10 22:34:33 +0000

    Move to supporting python3.6+ only as prior versions are have reached end-of-life

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
+import importlib
 import os
+import sys
+import warnings
 
 from setuptools import setup
+
+if sys.version_info[0:2] < (3, 6):
+    warnings.warn('This package is tested with Python version 3.6+')
 
 root_path = os.path.abspath(os.path.dirname(__file__))
 
@@ -11,17 +17,19 @@ with open(os.path.join(root_path, 'README.rst')) as readme:
 install_requires = ['Django>=2.2,<4', 'requests']
 tests_require = ['flake8', 'responses']
 
+package_info = importlib.import_module('moj_irat')
+
 setup(
     name='django-moj-irat',
-    version='0.5',
-    author='Ministry of Justice Digital & Technology',
+    version=package_info.__version__,
+    author=package_info.__author__,
     author_email='dev@digital.justice.gov.uk',
     url='https://github.com/ministryofjustice/django-moj-irat',
     packages=['moj_irat'],
     include_package_data=True,
     license='MIT',
-    description="Tools to support adding a Django-based service to "
-                "Ministry of Justice's Incidence Response and Tuning",
+    description='Tools to support adding a Django-based service to '
+                'Ministry of Justice’s Incidence Response and Tuning',
     long_description=README,
     keywords='moj django irat monitoring',
     classifiers=[
@@ -32,7 +40,6 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
